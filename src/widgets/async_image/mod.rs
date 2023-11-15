431cea35629415395c89b3e3f3a34530644ca9ed
Quark97 <quark97@proton.me>
2023-11-14 19:02:10 -0500

    Greatly improve AsyncImage performance

--- src/widgets/async_image/mod.rs
+++ src/widgets/async_image/mod.rs
@@ -1,9 +1,13 @@
 use adw::prelude::*;
 use adw::subclass::prelude::*;
 
-use glib::{Object, MainContext, Priority, Properties};
-use gtk::{gdk_pixbuf::{Pixbuf, InterpType}, CompositeTemplate};
+use glib::{MainContext, Object, Priority, Properties};
+use gtk::{
+    gdk_pixbuf::{Pixbuf, PixbufLoader},
+    CompositeTemplate,
+};
 use gtk::{gio, glib};
+use isahc::prelude::*;
 use std::cell::{Cell, RefCell};
 
 mod imp {
@@ -67,45 +71,34 @@ mod imp {
     impl BinImpl for AsyncImage {}
 
     impl AsyncImage {
-        fn set_uri(&self, value: String) {
+        fn set_uri(&self, uri: String) {
             // When initializing the object it is set to nothing
             // Ignore this when it happens
-            if value.is_empty() {
+            if uri.is_empty() {
                 return;
             }
 
             MainContext::default().spawn_local(
-                glib::clone!(@strong value, @weak self as _self => async move {
-                    let file = gio::File::for_uri(&value);
-                    match file.read_future(Priority::default()).await {
-                        Ok(stream) => {
-                            if let Ok(pixbuf) = Pixbuf::from_stream_future(&stream).await {
-                                let width = _self.obj().width();
-                                let height = _self.obj().height();
+                glib::clone!(@strong uri, @weak self as _self => async move {
+                    if let Ok(mut response) = isahc::get_async(uri).await {
+                        if response.status().is_success() {
+                            if let Ok(image_data) = response.bytes().await {
+                                let pixbuf_loader = PixbufLoader::new();
+                                if pixbuf_loader.write(image_data.as_slice()).is_ok() {
+                                    pixbuf_loader.close().unwrap();
+                                    let pixbuf = pixbuf_loader.pixbuf().unwrap();
 
-                                let pixbuf = if width > 0 && height > 0 {
-                                    if let Some(pixbuf) = pixbuf.scale_simple(_self.obj().width(), _self.obj().height(), InterpType::Nearest) {
-                                        pixbuf
-                                    } else {
-                                        pixbuf
-                                    }
-                                } else {
-                                    pixbuf
-                                };
-
-                                _self.picture.set_pixbuf(Some(&pixbuf));
-                                _self.picture.set_width_request(width);
-                                _self.picture.set_height_request(height);
-                                _self.stack.set_visible_child_name("picture");
-                                _self.spinner.set_spinning(false);
-                                _self.spinner.stop();
-                            } 
-                        },
-                        Err(error) => {
-                            _self.stack.set_visible_child_name("error");
-                            println!("{}: {:?}", value, error);
+                                    // Set the Pixbuf on the Image widget
+                                    _self.picture.set_pixbuf(Some(&pixbuf));
+                                    _self.stack.set_visible_child_name("picture");
+                                    _self.spinner.set_spinning(false);
+                                    _self.spinner.stop();
+                                    return;
+                                }
+                            }
                         }
                     }
+                    _self.stack.set_visible_child_name("error");
                 }),
             );
         }
